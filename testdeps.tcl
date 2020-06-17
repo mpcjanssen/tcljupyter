86ca0d1df0bde5d6c668017206150b75c88b640c
Mark Janssen <mpc.janssen@gmail.com>
2020-06-17 10:42:35 +0200

    Update for binder

--- testdeps.tcl
+++ testdeps.tcl
@@ -2,5 +2,5 @@ package require Tcl 8.6
 package require Thread
 package require rl_json
 package require zmq
-puts [glob $env(PREFIX)/share/jupyter/kernels/tclkernel/*]
+puts [glob $env(PREFIX)/share/jupyter/kernels/tcljupyter/*]
 
