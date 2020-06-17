a49e941b583552b0f15f69ba63e469eb9ab4485b
Mark Janssen <mpc.janssen@gmail.com>
2020-06-17 11:10:32 +0200

    Update

--- init.tcl
+++ init.tcl
@@ -3,6 +3,13 @@ lappend auto_path [file join [file dirname [info script]] libs-$tcl_platform(pla
 
 package require Tcl 8.6
 package require tclkernel
+if {[llength $argv] == 0} {
+  puts $auto_path
+  puts [package require Thread]
+  puts [package require rl_json]
+  puts [package require zmq]
+  exit 1
+}
 lassign $argv connection_file
 puts ">>>>>>>>>>>>>>>>>>>>>>>>> Kernel started, pid: [pid] <<<<<<<<<<<<<<<<<<<<<<<<<"
 connect $connection_file
--- testdeps.tcl
+++
@@ -1,5 +0,0 @@
-package require Tcl 8.6
-package require Thread
-package require rl_json
-package require zmq
-
