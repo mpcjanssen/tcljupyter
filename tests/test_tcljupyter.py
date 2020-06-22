e144906642a72f815b71154847de4a9f1369d520
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2020-06-22 14:05:50 +0200

    tests: Started working on Jupyter Kernel Testing suite

---
+++ tests/test_tcljupyter.py
@@ -0,0 +1,34 @@
+import unittest
+import jupyter_kernel_test as jkt
+
+class TclJupyterTests(jkt.KernelTests):
+    # Required --------------------------------------
+
+    # The name identifying an installed kernel to run the tests against
+    kernel_name = "tcljupyter"
+
+    # language_info.name in a kernel_info_reply should match this
+    language_name = "tcl"
+
+    # Optional --------------------------------------
+
+    # Code in the kernel's language to write "hello, world" to stdout
+    code_hello_world = 'puts "hello, world"'
+
+    # Pager: code that should display something (anything) in the pager
+    # code_page_something = "help(something)"
+
+    # Samples of code which generate a result value (ie, some text
+    # displayed as Out[n])
+    code_execute_result = [
+        {'code': 'expr {6 * 7}', 'result': '42'}
+    ]
+
+    # Samples of code which should generate a rich display output, and
+    # the expected MIME type
+    # code_display_data = [
+    #    {'code': 'show_image()', 'mime': 'image/png'}
+    #]
+
+if __name__ == '__main__':
+    unittest.main()
