d99542e8156eee910e802bf0cd6bac27b0cfe8de
Mark Janssen <mpc.janssen@gmail.com>
2025-10-02 13:24:22 +0200

    Switched to sqlite3 so now works for tcl 8 and 9

--- poc/testns.tcl
+++
@@ -1,9 +0,0 @@
-package require Thread
-
-set t [thread::create]
-
-thread::send -async $t "interp create slave ; slave eval {package require http}; thread::wait "
-thread::send -async $t "slave eval {http::geturl http://www.google.com}"
-
-thread::send -async $t {slave eval {puts [namespace current]}}
-vwait forever
